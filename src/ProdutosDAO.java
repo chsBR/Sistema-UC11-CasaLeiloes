37b85803c69d7e5d3e0389d09bff4ab86c5ce9fd
chsBR <2004starklipe@gmail.com>
2026-01-17 16:35:53 -0300

    Implementa listagem de dados para exibição na tabela

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -42,6 +42,30 @@ public class ProdutosDAO {
     }
 
     public ArrayList<ProdutosDTO> listarProdutos() {
+        String sql = "SELECT * FROM produtos";
+
+        conn = new conectaDAO().connectDB();
+
+        try {
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+
+            listagem.clear();
+
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+
+                listagem.add(produto);
+            }
+
+        } catch (Exception erro) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar produtos: " + erro);
+        }
 
         return listagem;
     }
